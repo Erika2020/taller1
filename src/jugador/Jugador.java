a6d3876245f4a79f71e3e2af36c0c5b21c8bbfcf
erika camacho <erica633@gmail.com>
2021-10-01 09:56:32 -0500

    se crea proyecto

---
+++ src/jugador/Jugador.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jugador;
+
+/**
+ *
+ * @author erica
+ */
+public class Jugador {
+    private int id;
+    private int Numcamisa;
+    private String nombre;
+    private String apellido;
+    private String posicion;
+    private int edad;
+    private String equipo;
+
+    public Jugador() {
+    }
+    
+    
+
+    public Jugador(int id, int Numcamisa, String nombre, String apellido, String posicion, int edad, String equipo) {
+        this.id = id;
+        this.Numcamisa = Numcamisa;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.posicion = posicion;
+        this.edad = edad;
+        this.equipo = equipo;
+    }
+     //GET Y SET
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumcamisa() {
+        return Numcamisa;
+    }
+
+    public void setNumcamisa(int Numcamisa) {
+        this.Numcamisa = Numcamisa;
+    }
+
+    public String getnombre() {
+        return nombre;
+    }
+
+    public void setnombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(String posicion) {
+        this.posicion = posicion;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(String equipo) {
+        this.equipo = equipo;
+    }
+    
+    
+    public int buscarNumcamisa(){
+ 
+        return(0);    
+    
+}
+}
---
+++ src/taller1/Taller1.java
@@ -0,0 +1,143 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package taller1;
+
+import java.util.Scanner;
+import jugador.Jugador;
+
+/**
+ *
+ * @author erica
+ */
+public class Taller1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int opcion;
+        int contador=0;
+        
+        Jugador[] jugadores=new Jugador[23];
+        do{
+            System.out.println("Bienvenidos");
+            System.out.println("***********");
+            System.out.println("1.Agregar Jugador");
+            System.out.println("2.Buscar Jugador");
+            System.out.println("3.Editar Jugador");
+            System.out.println("4.Mostar Jugadores");
+            System.out.println("5.Salir");
+            
+            System.out.println("Digite una opcion");
+            opcion=entrada.nextInt();
+            
+            switch(opcion){
+                case 1:
+                    jugadores[contador]=new Jugador();
+                    
+                    System.out.println("Digite id:");
+                    jugadores[contador].setId(entrada.nextInt());
+                    System.out.println("Digite Numcamisa");
+                    jugadores[contador].setNumcamisa(entrada.nextInt());
+                    System.out.println("Digite nombre:");
+                    jugadores[contador].setnombre(entrada.next());
+                    System.out.println("Digite Apellido:");
+                    jugadores[contador].setApellido(entrada.next());
+                    System.out.println("Digite Posicion:");
+                    jugadores[contador].setPosicion(entrada.next());
+                    System.out.println("Digite Edad:");
+                    jugadores[contador].setEdad(entrada.nextInt());
+                    System.out.println("Digite Equipo:");
+                    jugadores[contador].setEquipo(entrada.next());
+                    contador++;
+                    
+                    break;  
+                case 2:
+                    boolean jugador = true;
+                    System.out.println("Digite el camiseta jugador a buscar");
+                    int camisetaabuscar=entrada.nextInt();
+                    
+                    for(int i=0; i<contador; i++){
+                        if(camisetaabuscar==jugadores[i].getNumcamisa()){
+                            System.out.println("el jugador existe:");
+                            
+                            break;
+                        }else{
+                            System.out.println("el jugador no existe:");
+                            break;
+                        }
+                    }
+                   
+                    
+                    break;
+          
+                case 3: 
+                   boolean Jugador = true;
+                    System.out.println("Digite el  jugador a editar");
+                    int editjugador =entrada.nextInt();
+                    
+                    for(int i=0; i<contador; i++){
+                        if(editjugador==jugadores[i].getNumcamisa()){
+                            System.out.println("Digite id:");
+                            jugadores[i].setId(entrada.nextInt());
+                            System.out.println("Digite Numcamisa");
+                            jugadores[i].setNumcamisa(entrada.nextInt());
+                            System.out.println("dita el nuevo nombre");
+                            jugadores[i].setnombre(entrada.next());
+                            System.out.println("Digite Apellido:");
+                            jugadores[i].setApellido(entrada.next());
+                            System.out.println("Digite Posicion:");
+                            jugadores[i].setPosicion(entrada.next());
+                            System.out.println("Digite Edad:");
+                            jugadores[i].setEdad(entrada.nextInt());
+                            System.out.println("Digite Equipo:");
+                            jugadores[i].setEquipo(entrada.next());
+                            
+                            
+                            
+                            
+                            
+                            break;
+                        }else{
+                            System.out.println("el jugador no editado:");
+                            break;
+                        }
+                    }
+                    
+                    break;
+                case 4:
+                    for(int i=0; i<contador; i++){
+                    System.out.println(jugadores[i].getId());    
+                    System.out.println(jugadores[i].getNumcamisa());
+                    System.out.println(jugadores[i].getnombre());
+                    System.out.println(jugadores[i].getApellido());
+                    System.out.println(jugadores[i].getPosicion());
+                    System.out.println(jugadores[i].getEdad());
+                    System.out.println(jugadores[i].getEquipo());
+                        
+                        
+                    }
+                    break;
+                case 5:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("opcion no valida");
+                    break;
+               
+                 
+            
+            
+            }
+            
+        }while(opcion!=5);{
+        
+        
+    }
+    }
+    
+}
